ef3fddb36e06960288dcdb850094d9ad20edd78a
aayanofficial5 <aayanofficial5@gmail.com>
2025-03-28 14:14:44 +0530

    cloudinary configuration update

--- Config/cloudinary.js
+++ Config/cloudinary.js
@@ -1,15 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
-exports.cloudinaryConnect = () => {
-  try {
-    cloudinary.config({
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.CLOUD_API_KEY,
-      api_secret: process.env.CLOUD_API_SECRET,
-    });
-    console.log("Cloudinary connected successfully");
-  } catch (err) {
-    console.log("Error connecting to Cloudinary:", err);
-  }
+exports.cloudinaryConfigure = () => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+  });
 };
