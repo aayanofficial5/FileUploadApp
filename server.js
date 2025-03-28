78eb0c3be2ad637f2246f7576cf6e6b3a7736b71
aayanofficial5 <aayanofficial5@gmail.com>
2025-03-28 14:20:22 +0530

    server updated

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const dbConnect = require("./Config/database");
-const router = require("./Routes/router");
-const app = express();
+const Upload = require("./Routes/FileUpload");
+const {cloudinaryConfigure} = require("./Config/cloudinary");
+const fileUpload = require("express-fileupload");
 require("dotenv").config();
+
+const app = express();
+
 
 const port = process.env.PORT || 4000; // Use PORT for the server
 
-// request body json data parser
-app.use(express.json());
+// middlewares
+app.use(express.json()); // request body json data parser
+app.use(fileUpload()); //  upload files on server 
+
 
 // mapping of routes with app
-app.use("/api/v1", router);
+app.use("/api/v1/upload", Upload);
 
 // server starting
 app.listen(port, () => {
   console.log(`server is running on port ${port}...`);
 });
 
+// cloudinary configuration
+cloudinaryConfigure();  // to upload files on media server using server
 // database connection
-dbConnect();
+dbConnect(); // to store data to server
