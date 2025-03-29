bfd2dea0c42507746bd6ccd736f851297a6711ae
aayanofficial5 <aayanofficial5@gmail.com>
2025-03-29 20:18:29 +0530

    send email functionality added

--- Models/File.js
+++ Models/File.js
@@ -1,24 +1,48 @@
 const mongoose = require("mongoose");
+const { createTransporter } = require("../Config/email");
+require('dotenv').config();
 
+// create a schema
 const fileSchema = new mongoose.Schema({
   // write data structure of schema
   name: {
     type: String,
-    required: true
+    required: true,
   },
-  imageUrl:{
-    type:String,
+  imageUrl: {
+    type: String,
   },
-  videoUrl:{
-    type:String,
+  videoUrl: {
+    type: String,
   },
-  tags:{
-    type:String,
+  tags: {
+    type: String,
   },
-  email:{
-    type:String,
-    required:true
-  } 
-})
+  email: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model("File",fileSchema);
+fileSchema.post("save", async (doc) => {
+  try {
+
+    //create a  transporter
+    
+    let transporter = createTransporter();
+
+    // send a mail over transporter
+    
+    const info = await transporter.sendMail({
+      from: "Aayan",
+      to: doc.email,
+      subject: "Images uploaded on cloud",
+      html: `<h1>Your Image is Successfully Uploaded to cloud</h1><br><br><a href=${doc.imageUrl}>Image Link</>`,
+    });
+    console.log("Mail sent successfully to "+doc.email);
+    console.log(info);
+  } catch (err) {
+    console.log(err);
+  }
+});
+module.exports = mongoose.model("File", fileSchema);
